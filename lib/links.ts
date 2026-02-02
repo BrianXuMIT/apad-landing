5d95fe8ccb7e3232a64f63aa3b3659ea4482291e
Brian Xu <brianxu.mit@gmail.com>
2026-02-01 19:52:59 -0600

    update portal url

--- lib/links.ts
+++ lib/links.ts
@@ -1,2 +1,2 @@
 export const BOOK_DEMO_URL = 'https://calendly.com/founder-apadcode/30min'
-export const REGISTER_URL = 'https://www.apadcode.com'
+export const REGISTER_URL = 'https://portal.apadcode.com'
